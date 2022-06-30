e79088ca6c59cf6b1fe160506d24166905a8af9c
Misael Lopes <mecl.ely@gmail.com>
2022-06-30 09:44:08 +0100

    Lesson 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-us-west-2.graphcms.com/v2/cl4o1400o049301z78zy62s3f/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
